4db2cecede1f38cd5e188d63e9e99b86a781f52b
Mike Ludemann <mike-ludemann@hotmail.de>
2019-10-07 16:42:33 +0200

    Setup complete - vue-slick

---
+++ src/components/slick.vue
@@ -0,0 +1,83 @@
+<template lang="html">
+  <section id="slick">
+
+  </section>
+</template>
+
+<script>
+import Vue from "vue";
+import $ from "jquery";
+
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick.min.js";
+
+const SlickSlider = Vue.component('SlickSlider', {
+  props: {
+    class: {
+      type: String
+    },
+    options: {
+      type: Object
+    }
+  },
+  mounted(){
+    $(document).ready(function(){
+      $(this.class).slick(this.options);
+    });
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  template: `
+    <div :class="class">
+      <slot></slot>
+    </div>
+  `
+});
+
+const SliderElement = Vue.component('SliderElement', {
+  props: {},
+  mounted(){
+    
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  template: `
+    <div>
+      <slot></slot>
+    </div>
+  `
+});
+
+export default  {
+  name: 'slick',
+  components: {
+    SlickSlider,
+    SliderElement
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .slick {
+
+  }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <SlickSlider 
+      class="main-slider"
+      options="{
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }"
+    >
+      <SliderElement>
+        1-1
+      </SliderElement>
+      <SliderElement>
+        2-1
+      </SliderElement>
+      <SliderElement>
+        3-1
+      </SliderElement>
+    </SlickSlider>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+import { SlickSlider, SliderElement } from '@/components/slick.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    SlickSlider,
+    SliderElement
+  },
+})
+export default class About extends Vue {}
+</script>
